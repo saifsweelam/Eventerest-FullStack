17c5df1910420437c5afe656063ae98c888673d1
Seif El-Din Sweilam <saifsweelam@gmail.com>
2025-05-09 01:03:15 +0300

    feat(web): implement authentication flow with login and registration pages

---
+++ apps/web/src/pages/auth/login/page.tsx
@@ -0,0 +1,56 @@
+import { useAuth } from "../../../providers/authProvider";
+
+const LoginPage = () => {
+    const authContext = useAuth();
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+
+        
+        // Call your login function here
+        console.log("Logging in with:", { email, password });
+        authContext.login({email, password});
+    }
+
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <div className="mb-4">
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition duration-200"
+                >
+                    Login
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default LoginPage;
---
+++ apps/web/src/providers/authProvider.tsx
@@ -0,0 +1,64 @@
+import React, { useContext, createContext } from "react";
+import { authClient } from "../lib/authClient";
+import type { User, Session } from "better-auth";
+import type { BetterFetchError } from "better-auth/react";
+
+export type LoginPayload = {
+    email: string;
+    password: string;
+}
+
+export type RegisterPayload = {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export type AuthProviderProps = {
+    login: (payload: LoginPayload) => void;
+    logout: () => void;
+    register: (payload: RegisterPayload) => void;
+    user?: User;
+    session?: Session;
+    isPending?: boolean;
+    error: BetterFetchError | null;
+};
+
+const AuthContext = createContext<AuthProviderProps>({
+    login: () => {},
+    logout: () => {},
+    register: () => {},
+    error: null,
+});
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const { data, isPending, error } = authClient.useSession();
+
+    const login = async (payload: LoginPayload) => {
+        return await authClient.signIn.email(payload);
+    };
+
+    const logout = async () => {
+        return await authClient.signOut();
+    }
+
+    const register = async (payload: RegisterPayload) => {
+        return await authClient.signUp.email(payload);
+    };
+
+    return <AuthContext.Provider value={{
+        login,
+        logout,
+        register,
+        user: data?.user,
+        session: data?.session,
+        isPending,
+        error,
+    }}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
