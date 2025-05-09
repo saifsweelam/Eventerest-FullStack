a878193843722721ff0011180062a2481662eea4
Seif El-Din Sweilam <saifsweelam@gmail.com>
2025-05-10 01:45:49 +0300

    feat(api): add request validation middleware using zod

---
+++ apps/api/src/middlewares/validate.middleware.ts
@@ -0,0 +1,34 @@
+import { RequestHandler } from "express";
+import { z } from "zod";
+
+export const validateRequest = <
+    P extends z.ZodSchema,
+    R extends z.ZodSchema,
+    B extends z.ZodSchema,
+    Q extends z.ZodSchema,
+>(schema: {
+    params?: P;
+    response?: R;
+    body?: B;
+    query?: Q;
+}): RequestHandler<
+    z.output<P>,
+    z.output<R>,
+    z.output<B>,
+    z.output<Q>
+> => (req, res, next) => {
+    
+    if (schema.params) {
+        req.params = schema.params.parse(req.params);
+    }
+
+    if (schema.query) {
+        req.query = schema.query.parse(req.query);
+    }
+
+    if (schema.body) {
+        req.body = schema.body.parse(req.body);
+    }
+
+    next();
+};
